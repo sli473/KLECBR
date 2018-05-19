65faa1bd8424a3856a2ec0044457cffb38f699d4
Sueyeon Lee <sueyeonlee96@gmail.com>
2018-05-19 15:32:24 +1200

    Added basic logistic regression functions and example data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
-import jcolibri.casebase.CachedLinealCaseBase;
+import weka.LogisticRegression;
+
 
 public class Main {
 
     public static void main (String[] args) {
         System.out.println("Hello world,");
+
+        LogisticRegression logisticRegression = new LogisticRegression();
+        try {
+            logisticRegression.process();
+        } catch (Exception e) {
+            System.out.println("Sam said he was retarded");
+        }
 
         CaseBaseGenerator cb = new CaseBaseGenerator();
         cb.generate();
--- src/main/java/weka/LogisticRegression.java
+++ src/main/java/weka/LogisticRegression.java
@@ -1,23 +1,26 @@
 package weka;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.functions.Logistic;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
 /**
+ * CODE ADAPTED FROM https://tech.io/playgrounds/4821/machine-learning-with-java---part-2-logistic-regression
+ * BY Gowgi
  * Created by sueyeon on 19/05/18.
  */
 public class LogisticRegression {
 
-    /** file names are defined*/
-    public static final String TRAINING_DATA_SET_FILENAME="weather.nominal.arff";
-    public static final String TESTING_DATA_SET_FILENAME="weather.nominal.test.arff";
-    public static final String PREDICTION_DATA_SET_FILENAME="weather.nominal-confused.arff";
+    public static final String TRAINING_DATA_SET_FILENAME="/data/mushrooms.arff";
+    public static final String TESTING_DATA_SET_FILENAME="/data/small-test.arff";
+    public static final String PREDICTION_DATA_SET_FILENAME="/data/mushrooms.arff";
 
     /**
      * This method is to load the data set.
@@ -26,25 +29,23 @@ public class LogisticRegression {
      * @throws IOException
      */
     public static Instances getDataSet(String fileName) throws IOException {
-        /**
-         * we can set the file i.e., loader.setFile("finename") to load the data
-         */
-        int classIdx = 1;
+
+        int classIndex = 0;
         /** the arffloader to load the arff file */
         ArffLoader loader = new ArffLoader();
-        //loader.setFile(new File(fileName));
+
         /** load the traing data */
-        loader.setSource(LogisticRegression.class.getResourceAsStream("/" + fileName));
+        InputStream inputStream = new FileInputStream(System.getProperty("user.dir") + fileName);
+        loader.setSource(inputStream);
         /**
          * we can also set the file like loader3.setFile(new
          * File("test-confused.arff"));
          */
         Instances dataSet = loader.getDataSet();
         /** set the index based on the data given in the arff files */
-        dataSet.setClassIndex(classIdx);
+        dataSet.setClassIndex(classIndex);
         return dataSet;
     }
-
 
     /**
      * This method is used to process the input and return the statistics.
@@ -56,9 +57,11 @@ public class LogisticRegression {
         Instances trainingDataSet = getDataSet(TRAINING_DATA_SET_FILENAME);
         Instances testingDataSet = getDataSet(TESTING_DATA_SET_FILENAME);
         /** Classifier here is Linear Regression */
-        Classifier classifier = new weka.classifiers.functions.Logistic();
+        Logistic classifier = new Logistic();
         /** */
         classifier.buildClassifier(trainingDataSet);
+
+        double[][] coefficients = classifier.coefficients();
         /**
          * train the alogorithm with the training data and evaluate the
          * algorithm with testing data
@@ -66,10 +69,18 @@ public class LogisticRegression {
         Evaluation eval = new Evaluation(trainingDataSet);
         eval.evaluateModel(classifier, testingDataSet);
         /** Print the algorithm summary */
-        System.out.println("** Linear Regression Evaluation with Datasets **");
-        System.out.println(eval.toSummaryString());
-        System.out.print(" the expression for the input data as per alogorithm is ");
+//        System.out.println("** Linear Regression Evaluation with Datasets **");
+//        System.out.println(eval.toSummaryString());
+//        System.out.print(" the expression for the input data as per alogorithm is ");
         System.out.println(classifier);
+//        System.out.println(classifier.getCapabilities());
+
+        for (int i = 0; i < coefficients.length; i++) {
+            System.out.println(coefficients[i][0]);
+        }
+
+        System.out.println(coefficients.length);
+        System.out.println(coefficients[0].length);
 
         Instance predicationDataSet = getDataSet(PREDICTION_DATA_SET_FILENAME).lastInstance();
         double value = classifier.classifyInstance(predicationDataSet);
